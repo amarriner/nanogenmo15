ddd972fcd43e4fa0da191b8e154024fe56dcf1c9
marrinea <marrinea@strose.edu>
2015-11-02 15:45:53 -0500

    Split index.js file out into a separate file

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
 (function () {
    'use strict';
 }());
-
-var govTrack = require('govtrack-node');
-
-govTrack.findRole({current: true }, function(err, res) {
-    if(! err) {
-        // console.log(JSON.stringify(res));
-    }
-});
-
-govTrack.findPerson({},
-    function(err, res) {
-        if (! err) {
-            for (var i in res.objects) {
-
-                var person = res.objects[i];
-
-                console.log(JSON.stringify(person).substring(0, 100));
-
-                if (person.twitterid !== null) {
-                    console.log(person.twitterid);
-                }
-                console.log('------------------------------------------------');
-            }
-        }
-    }
-);
